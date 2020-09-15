949bb1b18ed34699b639bdf839288c835193321b
Jared Echols <jechols@mavenlink.com>
2020-09-15 08:56:14 -0600

    add and contain methods added to linked_list

--- src/data/structures/list/linked_list.py
+++ src/data/structures/list/linked_list.py
@@ -1,8 +1,46 @@
 class LinkedList:
-    def __init__(self, key=None, value=None):
+    def __init__(self, data=None):
         self.length = 0
+        if data:
+            self.create_first_node(data)
+        else:
+            self.head = self.tail = None
 
-    def add(self, key, value):
-        self.length = 2
+    def add_last(self, data):
+        if self.head:
+            node = Node(data, prev_node=self.tail)
+            self.tail.next = node
+            self.tail = node
+            self.length += 1
+        else:
+            self.create_first_node(data)
 
+    def add_first(self, data):
+        if self.head:
+            node = Node(data, next_node=self.head)
+            self.head.previous = node
+            self.head = node
+            self.length += 1
+        else:
+            self.create_first_node(data)
 
+    def contains(self, key):
+        return self.find_node(key) is not None
+
+    def find_node(self, data):
+        node = self.head
+        while node and node.data != data:
+            node = node.next
+
+        return node if node else None
+
+    def create_first_node(self, data):
+        self.head = Node(data)
+        self.tail = self.head
+        self.length = 1
+
+class Node:
+    def __init__(self, data, next_node=None, prev_node=None):
+        self.data = data
+        self.next = next_node
+        self.previous = prev_node
--- test/data/structures/list/test_linked_list.py
+++ test/data/structures/list/test_linked_list.py
@@ -1,14 +1,26 @@
-from src.data.structures.list import linked_list
+from src.data.structures.list.linked_list import LinkedList
 
 
 class TestLinkedList:
     def setup_method(self, test_method):
-        self.linked_list = linked_list.LinkedList()
+        self.linked_list = LinkedList()
 
     def teardown_method(self, test_method):
         self.linked_list = None
 
-    def test_add(self):
-        self.linked_list.add(key="hello", value="goodbye")
-        self.linked_list.add(key="john", value="smith")
+    def test_add_last(self):
+        assert self.linked_list.length == 0
+        self.linked_list.add_last("amy")
+        self.linked_list.add_last("veronica")
         assert self.linked_list.length == 2
+        assert self.linked_list.contains("amy")
+        assert self.linked_list.contains("veronica")
+        assert not self.linked_list.contains("sally")
+        assert self.linked_list.head.data == "amy"
+        assert self.linked_list.tail.data == "veronica"
+
+    def test_add_first(self):
+        self.linked_list.add_first("amy")
+        self.linked_list.add_first("veronica")
+        assert self.linked_list.head.data == "veronica"
+        assert self.linked_list.tail.data == "amy"
